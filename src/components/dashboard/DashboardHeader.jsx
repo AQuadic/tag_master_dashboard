4cc311f5a697ae38869e7338677b00455f59d1ea
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-06-10 14:52:41 +0300

    Add filter

--- src/components/dashboard/DashboardHeader.jsx
+++ src/components/dashboard/DashboardHeader.jsx
@@ -1,17 +1,61 @@
-import { useState } from "react"
+import { Calendar } from "@/components/ui/calendar"
+import React, { useState } from "react"
 import Filter from "../icons/dashboard/Filter"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import Calender from "../icons/dashboard/Calender"
 
 const DashboardHeader = () => {
     const [activeFilter, setActiveFilter] = useState("Weekly")
     const timeFilters = ["Weekly", "Monthly", "Annually"]
+    const [date, setDate] = React.useState(new Date())
 
     return (
         <section>
             <div className="flex flex-wrap items-center gap-5">
-                <div className="py-3 px-6 border-[0.88px] border-[#000000] rounded-[7px] flex items-center justify-center gap-3">
-                    <Filter />
-                    <p className="text-[#000000] text-[21.02px] font-medium">filter</p>
-                </div>
+                <Dialog>
+                    <DialogTrigger>
+                        <div className="py-3 px-6 border-[0.88px] border-[#000000] rounded-[7px] flex items-center justify-center gap-3">
+                            <Filter />
+                            <p className="text-[#000000] text-[21.02px] font-medium">filter</p>
+                        </div>
+                    </DialogTrigger>
+                    <DialogContent className='md:w-[410px] w-full'>
+                        <DialogHeader>
+                            <DialogTitle className='text-[#002F10] texet-[22px] font-semibold'>Filter by</DialogTitle>
+                            <DialogDescription>
+                                <div className="flex items-center justify-between mt-6">
+                                    <div className="px-5 py-3 border border-[#007EC1] bg-[#F6F6F6] rounded-[12px] flex items-center gap-2">
+                                        <Calender />
+                                        <p className="text-[#007EC1] text-lg font-medium">2 July 2024</p>
+                                    </div>
+                                    <div className="px-5 py-3 border border-[#007EC1] bg-[#F6F6F6] rounded-[12px] flex items-center gap-2">
+                                        <Calender />
+                                        <p className="text-[#007EC1] text-lg font-medium">2 July 2024</p>
+                                    </div>
+                                </div>
+                                <div className="mt-6">
+                                    <Calendar
+                                        mode="single"
+                                        selected={date}
+                                        onSelect={setDate}
+                                        className="rounded-md border shadow-sm"
+                                        captionLayout="dropdown"
+                                    />
+                                </div>
+                                <div className="mt-5 bottom-0">
+                                    <button className="w-full h-[64px] bg-[#000000] text-[#FFFFFF] text-xl font-medium">تطبيق</button>
+                                </div>
+                            </DialogDescription>
+                        </DialogHeader>
+                    </DialogContent>
+                </Dialog>
                 {timeFilters.map((filter) => (
                     <div
                         key={filter}
