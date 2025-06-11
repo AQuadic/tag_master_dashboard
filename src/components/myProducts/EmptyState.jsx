bf65804de69b9aa4283e6aff83e12deb37dd9034
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-06-11 14:38:48 +0300

    my products section responsive

--- src/components/myProducts/EmptyState.jsx
+++ src/components/myProducts/EmptyState.jsx
@@ -4,8 +4,8 @@ const EmptyState = () => {
     return (
         <div className="flex flex-col items-center justify-center mt-32">
             <img src={emptyCart} alt="Empty cart" />
-            <h2 className="text-[#9C9C9C] text-[28px] font-medium text-center">You have no products yet.</h2>
-            <p className="text-[#000000] text-[22px] font-medium mt-[10px]">New products appear here.</p>
+            <h2 className="text-[#9C9C9C] md:text-[28px] text-lg font-medium text-center">You have no products yet.</h2>
+            <p className="text-[#000000] md:text-[22px] text-base font-medium mt-[10px] text-center">New products appear here.</p>
         </div>
     )
 }
--- src/components/myProducts/Products.jsx
+++ src/components/myProducts/Products.jsx
@@ -28,24 +28,24 @@ const Products = () => {
                     <div
                         key={products}
                         onClick={() => setActiveProducts(products)}
-                        className={`py-3 px-6 border-[0.88px] border-[#000000] rounded-[7px] flex items-center justify-center cursor-pointer transition-colors ${activeProducts === products
+                        className={`md:py-3 md:px-6 p-1 border-[0.88px] border-[#000000] rounded-[7px] flex items-center justify-center cursor-pointer transition-colors ${activeProducts === products
                             ? "bg-[#002847] text-white border-none"
                             : "bg-transparent text-[#000000]"
                             }`}
                     >
-                        <p className="text-[21.02px] font-medium">{products}</p>
+                        <p className="md:text-[21.02px] text-base font-medium">{products}</p>
                     </div>
                 ))}
             </div>
             <div className="mt-[26px] flex flex-wrap items-center gap-[42px]">
                 {cardData.map((product, index) => (
-                    <div key={index} className="w-[275px]">
+                    <div key={index} className="md:w-[275px] w-full">
                         <img
                             src={product.image}
                             alt="Product"
                             className="w-full rounded-tr-[12px] rounded-tl-[12px]"
                         />
-                        <div className="h-[58px] border-b border-r border-l border-[#000000] rounded-b-[12px] px-3">
+                        <div className="h-full pb-2 border-b border-r border-l border-[#000000] rounded-b-[12px] px-3">
                             <p className="text-[#000000] text-xs pt-1.5">{product.title}</p>
                             <div className="flex items-center gap-6 mt-2">
                                 <div className="flex items-center gap-2">
@@ -83,7 +83,7 @@ const Products = () => {
 
 
             <div className="flex items-center justify-center mt-32">
-                <button className="w-[255px] h-[59px] bg-[#002847] rounded-[12px] text-[#FFFFFF] text-2xl font-medium">Buy a product</button>
+                <button className="md:w-[255px] w-full h-[59px] bg-[#002847] rounded-[12px] text-[#FFFFFF] text-2xl font-medium">Buy a product</button>
             </div>
         </section>
     )
