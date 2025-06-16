5c2c1cc11b0fd59b57c442b360241f8ac32bb62e
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-06-16 13:41:22 +0300

    add "active" in edit links dialog

--- src/components/myAccount/EditLink.jsx
+++ src/components/myAccount/EditLink.jsx
@@ -26,6 +26,16 @@ const EditLink = () => {
                 </label>
             </div>
 
+            <div className="w-full h-[72px] flex items-center justify-between border border-[#979797] rounded-[35px] mt-10 p-6">
+                <div className="flex items-center gap-1.5">
+                    <p className="text-[#000000] text-base">Active</p>
+                </div>
+                <label className="inline-flex items-center cursor-pointer">
+                    <input type="checkbox" value="" className="sr-only peer" />
+                    <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+                </label>
+            </div>
+
             <div className="mt-12">
                 <label htmlFor="address" className="text-[#000000] text-base">Link Address</label>
                 <input
