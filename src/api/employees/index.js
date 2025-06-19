ab1e1fc6d5cfef81f625a358f71be7c0bfd050fe
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-06-19 10:50:25 +0300

    integrate active employees

---
+++ src/api/employees/index.js
@@ -0,0 +1,6 @@
+import { axios } from "@/components/lib/axios";
+
+export const getEmployees = async () => {
+    const response = await axios.get("/dashboard/employees");
+    return response.data.profiles;
+};
--- src/components/dashboard/ActiveEmployee.jsx
+++ src/components/dashboard/ActiveEmployee.jsx
@@ -1,59 +1,27 @@
-import Employee1 from '../../../public/images/Dashboard/employee1.png'
-import Employee2 from '../../../public/images/Dashboard/employee2.png'
-import Employee3 from '../../../public/images/Dashboard/employee3.png'
-import Employee4 from '../../../public/images/Dashboard/employee4.png'
-import Employee5 from '../../../public/images/Dashboard/employee5.png'
-import Employee6 from '../../../public/images/Dashboard/employee6.png'
+import { useQuery } from 'react-query'
+import Employee1 from '/images/Dashboard/employee1.png'
 import ActiveStatus from '../icons/dashboard/ActiveStatus'
+import { getEmployees } from '@/api/employees'
+import Spinner from '../icons/general/Spinner'
 
 const ActiveEmployee = () => {
-    const EmployeeData = [
-        {
-            image: Employee1,
-            name: 'Mohamed reda',
-            title: 'Team leader',
-            activeStatus: true
-        },
-        {
-            image: Employee2,
-            name: 'Walied sayed',
-            title: 'HR',
-            activeStatus: true
-        },
-        {
-            image: Employee3,
-            name: 'Hatem adel',
-            title: 'Marketing Manager',
-            activeStatus: true
-        },
-        {
-            image: Employee4,
-            name: 'Ahmed attaia',
-            title: 'Sales Manager',
-            activeStatus: false
-        },
-        {
-            image: Employee5,
-            name: 'Mohamed reda',
-            title: 'Team leader',
-            activeStatus: true
-        },
-        {
-            image: Employee6,
-            name: 'Seif naser',
-            title: 'supervisor',
-            activeStatus: false
-        },
-    ]
+    const { data: employees = [], isLoading } = useQuery({
+        queryKey: ["employees"],
+        queryFn: getEmployees,
+    });
+
+    if (isLoading) {
+        return <Spinner />
+    }
 
     return (
         <section className="md:w-[360px] h-full border border-[#000000] rounded-2xl flex flex-col md:p-8 p-2 mt-4 md:mt-0">
             <h1 className="text-[#000000] md:text-2xl text-base font-medium mx-auto">Active employees</h1>
             <div className='mt-5 flex flex-col gap-8'>
-                {EmployeeData.map((employee, index) => (
+                {employees.map((employee, index) => (
                     <div key={index} className='flex items-center gap-[11px]'>
                         <div className="relative">
-                            <img src={employee.image} alt="Employee" />
+                            <img src={Employee1} alt="Employee" />
                             {employee.activeStatus && (
                                 <div className="absolute top-9 -right-1">
                                     <ActiveStatus />
@@ -62,7 +30,7 @@ const ActiveEmployee = () => {
                         </div>
                         <div>
                             <h2 className="text-[#000000] text-base font-medium">{employee.name}</h2>
-                            <p className="text-[#878787] md:text-base text-xs font-medium">{employee.title}</p>
+                            <p className="text-[#878787] md:text-base text-xs font-medium">{employee.description}</p>
                         </div>
                     </div>
                 ))}
