4e74619993edb6175e24f1ae143b5ee1ef09a330
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-06-19 14:04:50 +0300

    integrates current subscription

--- src/api/subscriptions/index.js
+++ src/api/subscriptions/index.js
@@ -4,3 +4,8 @@ export const getSubscriptions = async () => {
     const response = await axios.get("/subscriptions");
     return response.data;
 };
+
+export const getCurrentSubscriptions = async () => {
+    const response = await axios.get("/subscriptions/current");
+    return response.data;
+};
--- src/components/paymentplan/Header.jsx
+++ src/components/paymentplan/Header.jsx
@@ -1,34 +1,44 @@
+import { useQuery } from "react-query"
 import ActiveStatus from "../icons/dashboard/ActiveStatus"
 import Pro from "../icons/paymentplan/Pro"
+import { getCurrentSubscriptions } from "@/api/subscriptions"
 
 const Header = () => {
+
+    const { data: currentSubscription } = useQuery({
+        queryKey: ["currentSubscriptions"],
+        queryFn: getCurrentSubscriptions,
+    });
+
     return (
         <section className="w-full h-full rounded-[12px] md:px-[60px] px-4 py-[18px]" style={{ boxShadow: '0px 1px 2px 0px #00000040' }}>
             <div className="flex lg:flex-row flex-col justify-between">
                 <div className="lg:mt-0 mt-4">
                     <h2 className="text-[#000000] text-xl font-medium">Package name</h2>
-                    <p className="flex items-center gap-4 lg:mt-[23px] mt-4">
+                    <p className="flex md:flex-row flex-col justify-between md:gap-4 lg:mt-[23px] mt-4">
                         <Pro />
-                        <span className="text-[#000000] text-xl font-normal">Companies</span>
+                        <span className="text-[#000000] md:text-xl text-base font-normal">
+                            {currentSubscription.plan?.name?.en ?? "N/A"}
+                        </span>
                     </p>
                 </div>
 
                 <div className="lg:mt-0 mt-4">
-                    <h2 className="text-[#000000] text-xl font-medium">Package name</h2>
+                    <h2 className="text-[#000000] text-xl font-medium">Status</h2>
                     <p className="flex items-center gap-4 lg:mt-[23px] mt-4">
-                        <span className="text-[#000000] text-xl font-normal">Active</span>
+                        <span className="text-[#000000] text-xl font-normal">{currentSubscription.status}</span>
                         <ActiveStatus />
                     </p>
                 </div>
 
                 <div className="lg:mt-0 mt-4">
                     <h2 className="text-[#000000] text-xl font-medium">Start date</h2>
-                    <p className="text-[#000000] text-xl font-normal lg:mt-[23px] mt-4">20\10\2025</p>
+                    <p className="text-[#000000] text-xl font-normal lg:mt-[23px] mt-4">{new Date(currentSubscription.start_date).toLocaleDateString()}</p>
                 </div>
 
                 <div className="lg:mt-0 mt-4">
                     <h2 className="text-[#000000] text-xl font-medium">Expiration date</h2>
-                    <p className="text-[#000000] text-xl font-normal lg:mt-[23px] mt-4">20\10\2027</p>
+                    <p className="text-[#000000] text-xl font-normal lg:mt-[23px] mt-4">{new Date(currentSubscription.end_date).toLocaleDateString()}</p>
                 </div>
             </div>
             <div className="flex items-center justify-center">
