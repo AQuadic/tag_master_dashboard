1fd58ea7d5b870fdbe467cf856d7cd4c2c27c4fc
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-06-24 15:07:15 +0300

    fetch analytics data

--- src/api/employees/index.js
+++ src/api/employees/index.js
@@ -4,3 +4,10 @@ export const getEmployees = async () => {
     const response = await axios.get("/dashboard/employees");
     return response.data.data || response.data;
 };
+
+
+
+export const getEmployeesAnalytics = async () => {
+        const response = await axios.get("/dashboard/employees?with_analytics=121");
+        return response.data.data || response.data;
+};
--- src/components/analytics/AnalyticsCards.jsx
+++ src/components/analytics/AnalyticsCards.jsx
@@ -1,9 +1,11 @@
-import { MembersData } from "@/constants/Analytics/MembersData"
 import Eye from "../icons/dashboard/Eye"
 import Hand from "../icons/dashboard/Hand"
 import LeftArrow from "../icons/analytics/LeftArrow"
 import RightArrow from "../icons/analytics/RightArrow"
 import { useState } from "react"
+import { useQuery } from "react-query"
+import { getEmployeesAnalytics } from "@/api/employees"
+import Spinner from "../icons/general/Spinner"
 
 const AnalyticsCards = () => {
 
@@ -18,29 +20,42 @@ const AnalyticsCards = () => {
         setCurrentPage(prev => Math.min(prev + 1, totalPages));
     };
 
+    const { data: analytics = [], isLoading } = useQuery({
+        queryKey: ["employees-analytics"],
+        queryFn: async () => {
+            const response = await getEmployeesAnalytics();
+            return response || [];
+        },
+    });
+
+    console.log("Analytics:", analytics);
+
+    if (isLoading) {
+        return <Spinner />;
+    }
 
     return (
         <section className="mt-10">
-            <div className="flex flex-wrap gap-6">
-                {MembersData.map((member, index) => {
+            <div className="grid 2xl:grid-cols-3 md:grid-cols-2  gap-6">
+                {analytics.map((member, index) => {
                     return (
-                        <div key={index} className="md:w-[293px] w-full h-full border border-[#000000] rounded-[8px] p-4">
+                        <div key={index} className="w-full h-full border border-[#000000] rounded-[8px] p-4">
                             <div className="flex md:flex-row flex-col items-center gap-2">
-                                <img src={member.image} alt="Employee" />
+                                <img src={member.image} alt="" className="w-[64px] h-[64px] object-cover rounded-full" />
                                 <div>
                                     <h1 className="text-[#000000] text-base font-medium">{member.name}</h1>
-                                    <p className="text-[#878787] text-base font-medium text-center md:text-start">{member.title}</p>
+                                    <p className="text-[#878787] text-base font-medium text-center md:text-start truncate w-[200px]">{member.email || "No email"}</p>
                                 </div>
                             </div>
                             <hr className="my-4" />
-                            <div className="flex flex-wrap items-center md:gap-8 gap-2">
+                            <div className="flex flex-wrap items-center md:gap-14 gap-2">
                                 <div className="flex items-center gap-2">
                                     <Eye />
-                                    <p className="text-[#5A5A5A] text-[22px] font-medium">{member.show}</p>
+                                    <p className="text-[#5A5A5A] text-[22px] font-medium">{member.basic_analytics?.views ?? 0}</p>
                                 </div>
                                 <div className="flex items-center gap-2">
                                     <Hand />
-                                    <p className="text-[#5A5A5A] text-[22px] font-medium">{member.click}</p>
+                                    <p className="text-[#5A5A5A] text-[22px] font-medium">{member.basic_analytics?.clicks ?? 0}</p>
                                 </div>
                             </div>
                         </div>
