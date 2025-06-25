45ccdc88f30797318b0bb411981483ca2005b715
ahmed <ahmedabozaid.dev@gmail.com>
2025-06-25 14:57:07 +0300

    updates login endpoint

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -1,23 +1,20 @@
 import { axios } from "@/components/lib/axios";
 
 export const postLogin = async ({ email, password }) => {
-    const response = await axios.post("/user/login", {
-        email,
-        password,
-        password_confirmation: password,
-    });
-    return response.data;
+  const response = await axios.post("/dashboard/login", {
+    email,
+    password,
+    password_confirmation: password,
+  });
+  return response.data;
 };
 
-
-export const postLogout= async () => {
-    const response = await axios.post("/logout", {
-    });
-    return response.data;
+export const postLogout = async () => {
+  const response = await axios.post("/logout", {});
+  return response.data;
 };
 
 export const deleteAccount = async () => {
-    const response = await axios.delete("/user/destroy", {
-    });
-    return response.data;
+  const response = await axios.delete("/user/destroy", {});
+  return response.data;
 };
